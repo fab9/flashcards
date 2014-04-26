59d9441dc600cb2d2df74ad47ad76d466024c2ba
Nate Kandler <natekandler@gmail.com>
2014-04-26 17:48:48 -0500

    adds login and user session

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,32 @@
+enable :sessions
+
 get '/' do
-  session[:user_id] = User.find(1)
+  # Look in app/views/index.erb
   erb :index
+end
+
+post '/' do
+  if User.authenticate(params[:username], params[:password])
+   @user = User.find_by_username(params[:username])
+    session[:user_id] = @user.id
+    redirect '/dashboard'
+  else
+    redirect '/'
+  end
 end
 
 get '/dashboard' do
   @decks = Deck.all
   erb :dashboard
+end
+
+get '/create_account' do
+  erb :create_account
+end
+
+post '/create_account' do
+  User.create(username: params[:username], password: params[:password])
+  redirect '/'
 end
 
 post '/deck_selection' do
@@ -18,4 +39,7 @@ post '/deck_selection' do
   redirect to ("/current_game")
 end
 
-
+get '/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
